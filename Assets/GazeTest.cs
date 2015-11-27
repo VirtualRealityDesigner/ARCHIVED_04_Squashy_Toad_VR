07e646f395576bb609f40653058944ed9a16356a
Sam Pattuzzi <sam@pattuzzi.it>
2015-11-27 17:12:13 +0000

    Overlay some UI text for getting gaze tracking info.

---
+++ Assets/GazeTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GazeTest : MonoBehaviour {
+
+    public Text debugText;
+
+    private CardboardHead cardboardHead;
+
+    // Use this for initialization
+    void Start () {
+        // print ("Looking for head...");
+        cardboardHead = GameObject.FindObjectOfType<CardboardHead>();
+        print (cardboardHead);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Debug.Log(cardboardHead.Gaze);
+        debugText.text = cardboardHead.Gaze.ToString();
+    }
+}
