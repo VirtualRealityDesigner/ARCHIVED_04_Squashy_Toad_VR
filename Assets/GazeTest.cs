5b1c27f73a76bd4b1fac6f0196b06e0c849cf407
Sam Pattuzzi <sam@pattuzzi.it>
2015-11-28 10:34:12 +0000

    VRB03 VR Gaze Tracking

---
+++ Assets/GazeTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeTest : MonoBehaviour {
+
+    private CardboardHead gazeTestHead;
+
+	// Use this for initialization
+	void Start () {
+        gazeTestHead = GameObject.FindObjectOfType<CardboardHead>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    print(gazeTestHead.Gaze);
+	}
+}
