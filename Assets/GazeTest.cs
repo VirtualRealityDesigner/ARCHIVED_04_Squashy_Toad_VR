7894ce64051fc08337d1332738d5e740f74ca94e
Sam Pattuzzi <sam@pattuzzi.it>
2015-11-28 13:30:34 +0000

    Do some rigidbody physics with the cube.

--- Assets/GazeTest.cs
+++ Assets/GazeTest.cs
@@ -7,16 +7,18 @@ public class GazeTest : MonoBehaviour {
     public Text gazeText;
 
     private CardboardHead gazeTestHead;
+    private Rigidbody rb; 
 
 	// Use this for initialization
 	void Start () {
         Cardboard.SDK.OnTrigger += PullTrigger;
         gazeTestHead = GameObject.FindObjectOfType<CardboardHead>();
+        rb = GetComponent<Rigidbody>();
 	}
 
     private void PullTrigger()
     {
-        gazeText.enabled = !gazeText.enabled;
+        rb.AddForce(gazeTestHead.Gaze.direction * 1000);
     }
 
     // Update is called once per frame
