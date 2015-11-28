8988f307d7ff5ed60e5b9f71f9b13b95ce8d5d87
Sam Pattuzzi <sam@pattuzzi.it>
2015-11-28 12:15:01 +0000

    Toggle text on trigger pull.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Text myText;
+
+	// Use this for initialization
+	void Start () {
+        Cardboard.SDK.OnTrigger += PullTrigger;
+	}
+
+    private void PullTrigger()
+    {
+        myText.enabled = !myText.enabled;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+}
