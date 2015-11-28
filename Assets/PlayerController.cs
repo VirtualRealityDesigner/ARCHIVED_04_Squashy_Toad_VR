b4fdc564805c0e3fe9753167b5e5720f86ef3f0f
Sam Pattuzzi <sam@pattuzzi.it>
2015-11-28 20:22:59 +0000

    WIP

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Text gazeText;
+
+    private CardboardHead gazeTestHead;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        Cardboard.SDK.OnTrigger += PullTrigger;
+        gazeTestHead = GameObject.FindObjectOfType<CardboardHead>();
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void PullTrigger()
+    {
+        rb.AddForce(gazeTestHead.Gaze.direction * 1000);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    gazeText.text = gazeTestHead.Gaze.ToString();
+	}
+}
