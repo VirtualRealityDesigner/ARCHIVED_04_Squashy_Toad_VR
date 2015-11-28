e8c35e192875a94c58ad10ad817aedb63c65a53b
Sam Pattuzzi <sam@pattuzzi.it>
2015-11-28 08:55:33 +0000

    Add some physics and movement part 1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private CardboardHead mainHead;
+
+	// Use this for initialization
+	void Start () {
+        Cardboard.SDK.OnTrigger += Jump;
+        mainHead = GameObject.FindObjectOfType<CardboardHead>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+
+    private void Jump()
+    {
+        gameObject.transform.Translate(mainHead.Gaze.direction);
+    }
+}
