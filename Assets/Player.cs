3ad4cb6d7bb0bed49d3edd53c8a10cc97388931d
Sam Pattuzzi <sam@pattuzzi.it>
2015-12-14 17:22:55 +0000

    VRB06 Adding Physics to VR Cameras

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,21 +2,23 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GazeTest : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public Text gazeText;
 
     private CardboardHead gazeTestHead;
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         Cardboard.SDK.OnTrigger += PullTrigger;
         gazeTestHead = GameObject.FindObjectOfType<CardboardHead>();
+        rb = GetComponent<Rigidbody>();
 	}
 
     private void PullTrigger()
     {
-        gazeText.enabled = !gazeText.enabled;
+        rb.AddForce(gazeTestHead.Gaze.direction * 1000);
     }
 
     // Update is called once per frame
