23158b76b99f10b08fccc63b4612517e1d559845
Sam Pattuzzi <sam@pattuzzi.it>
2015-12-15 19:26:23 +0000

    Only jump when on the ground.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour {
 
     private CardboardHead head;
     private Rigidbody rb;
+    private bool onGround;
 
 	// Use this for initialization
 	void Start () {
@@ -18,14 +19,27 @@ public class Player : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
 	}
 
-    private void PullTrigger()
-    {
-        Vector3 planeProjectedGaze = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
-        Vector3 unitPlaneProjectedGaze = Vector3.Normalize(planeProjectedGaze);
-        float jumpAngleInRadians = jumpAngleInDegrees * Mathf.Deg2Rad;
-        Vector3 unitJumpVelocity = Vector3.RotateTowards(unitPlaneProjectedGaze, Vector3.up, jumpAngleInRadians, 0);
-        Vector3 jumpVelocity = unitJumpVelocity * jumpSpeed;
-        rb.velocity = jumpVelocity;
+    private void PullTrigger() {
+        Jump();
+    }
+
+    private void Jump() {
+        if (onGround) {
+            Vector3 planeProjectedGaze = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+            Vector3 unitPlaneProjectedGaze = Vector3.Normalize(planeProjectedGaze);
+            float jumpAngleInRadians = jumpAngleInDegrees * Mathf.Deg2Rad;
+            Vector3 unitJumpVelocity = Vector3.RotateTowards(unitPlaneProjectedGaze, Vector3.up, jumpAngleInRadians, 0);
+            Vector3 jumpVelocity = unitJumpVelocity * jumpSpeed;
+            rb.velocity = jumpVelocity;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        onGround = true;
+    }
+
+    void OnCollisionExit(Collision collision) {
+        onGround = false;
     }
 
     // Update is called once per frame
