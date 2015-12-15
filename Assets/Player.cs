35f94bf5be4ccc3a86919b75098d6979c4a23bc6
Sam Pattuzzi <sam@pattuzzi.it>
2015-12-15 18:30:46 +0000

    Make jumping natural with instantaneous velocity.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,24 +5,31 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public Text gazeText;
+    public float jumpAngleInDegrees;
+    public float jumpSpeed;
 
-    private CardboardHead gazeTestHead;
+    private CardboardHead head;
     private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         Cardboard.SDK.OnTrigger += PullTrigger;
-        gazeTestHead = GameObject.FindObjectOfType<CardboardHead>();
+        head = GameObject.FindObjectOfType<CardboardHead>();
         rb = GetComponent<Rigidbody>();
 	}
 
     private void PullTrigger()
     {
-        rb.AddForce(gazeTestHead.Gaze.direction * 1000);
+        Vector3 planeProjectedGaze = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+        Vector3 unitPlaneProjectedGaze = Vector3.Normalize(planeProjectedGaze);
+        float jumpAngleInRadians = jumpAngleInDegrees * Mathf.Deg2Rad;
+        Vector3 unitJumpVelocity = Vector3.RotateTowards(unitPlaneProjectedGaze, Vector3.up, jumpAngleInRadians, 0);
+        Vector3 jumpVelocity = unitJumpVelocity * jumpSpeed;
+        rb.velocity = jumpVelocity;
     }
 
     // Update is called once per frame
     void Update () {
-	    gazeText.text = gazeTestHead.Gaze.ToString();
+	    gazeText.text = head.Gaze.ToString();
 	}
 }
