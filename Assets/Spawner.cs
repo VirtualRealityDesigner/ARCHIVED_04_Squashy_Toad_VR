5de1a6cb182b1a46e3b2aa07e21999109346dbbe
Sam Pattuzzi <sam@pattuzzi.it>
2015-12-16 21:15:32 +0000

    Ben's improvements/suggestions.

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,26 +4,27 @@ using System.Collections;
 public class Spawner : MonoBehaviour {
 
     public GameObject[] lanePrefabs;
-    public float laneWidth;
-    public float respawnMargin;
+    public float laneWidth = 2f;
+    public float spawnHorizon = 500f;
     public GameObject player;
+    public Transform parentTransform;
 
     private float startOffset = 0;
 
     // Use this for initialization
     void Start () {
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         float playerForwardPosition = player.transform.position.z;
-        while (playerForwardPosition > (startOffset - respawnMargin)) 
+        while (playerForwardPosition > (startOffset - spawnHorizon)) 
         {
             int randomLane = Random.Range(0, lanePrefabs.Length);
-            print(randomLane);
-            Vector3 startPosition = Vector3.zero + Vector3.forward * startOffset;
-            Instantiate(lanePrefabs[randomLane], startPosition, Quaternion.identity);
+            Vector3 startPosition = Vector3.forward * startOffset;
+            GameObject newLane = Instantiate(lanePrefabs[randomLane], startPosition, Quaternion.identity) as GameObject;
+            newLane.transform.parent = parentTransform;
             startOffset += laneWidth;
         }
     }
