00978c6015b32c33201b1f3e69ab7fd1ce1ecc1a
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2015-12-27 14:41:43 +0000

    VRB09 Procedurally generating terrain

---
+++ Assets/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] lanePrefabs;
+
+	// Use this for initialization
+	void Start () {
+        GameObject lane = lanePrefabs[0];
+        Instantiate(lane, Vector3.zero, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
