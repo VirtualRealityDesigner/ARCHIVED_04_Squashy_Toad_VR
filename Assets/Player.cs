d7bf976dcf10e6121e2585575ad9a65655aae9c8
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-01 16:16:26 +0000

    VRB11 Checking a Rigidbody is Grounded

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour {
     private CardboardHead head;
     private Rigidbody rb;
     private bool onFloor;
+    private float lastJumpRequestTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,23 +21,31 @@ public class Player : MonoBehaviour {
 
     private void PullTrigger()
     {
-        if (onFloor)
+        RequestJump();
+    }
+
+    private void RequestJump()
+    {
+        lastJumpRequestTime = Time.time;
+        rb.WakeUp();
+    }
+
+    private void Jump()
+    {
+        float jumpAngleInRadians = jumpAngleInDegree * Mathf.Deg2Rad;
+        Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+        Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
+        rb.velocity = jumpVector * jumpSpeed;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        float delta = Time.time - lastJumpRequestTime;
+        if (delta < 0.1)
         {
-            float jumpAngleInRadians = jumpAngleInDegree * Mathf.Deg2Rad;
-            Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
-            Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
-            rb.velocity = jumpVector * jumpSpeed;
+            Jump();
+            lastJumpRequestTime = 0.0f;
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        onFloor = true;
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        onFloor = false;
     }
 
     // Update is called once per frame
