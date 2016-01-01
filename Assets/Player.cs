664b738ab9134f43da042593ec66cac7c7e81472
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-01 12:53:17 +0000

    Fix the jumping with a timer and notification based system.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour {
     private CardboardHead head;
     private Rigidbody rb;
     private int collisionCount;
+    private CapsuleCollider collider;
+    private float lastJumpRequestTime = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,28 +22,35 @@ public class Player : MonoBehaviour {
 
     private void PullTrigger()
     {
-        print("PullTrigger");
-        if (collisionCount > 0)
+        RequestJump();
+    }
+
+    private void RequestJump()
+    {
+        lastJumpRequestTime = Time.time;
+        rb.WakeUp();
+        print("Jump requested at " + lastJumpRequestTime);
+    }
+
+    private void Jump()
+    {
+        print("Jumping");
+        float jumpAngleInRadians = jumpAngleInDegree * Mathf.Deg2Rad;
+        Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+        Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
+        rb.velocity = jumpVector * jumpSpeed;
+    }
+
+    public void OnCollisionStay(Collision collision)
+    {
+        float now = Time.time;
+        float delta = now - lastJumpRequestTime;
+        print("Collision stay at " + now + " with delta " + delta);
+        if (delta < 0.1)
         {
-            float jumpAngleInRadians = jumpAngleInDegree * Mathf.Deg2Rad;
-            Vector3 projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
-            Vector3 jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
-            rb.velocity = jumpVector * jumpSpeed;
+            Jump();
+            lastJumpRequestTime = 0;
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        print("OnCollisionEnter" + collision.collider.GetHashCode());
-        collisionCount++;
-        print(collisionCount);
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        print("OnCollisionExit" + collision.collider.GetHashCode());
-        collisionCount--;
-        print(collisionCount);
     }
 
     // Update is called once per frame
