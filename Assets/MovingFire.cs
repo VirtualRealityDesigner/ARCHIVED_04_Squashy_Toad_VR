b2b7c86c1f6c7b7939e53d90e447957ffb8fe70c
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-05 20:34:50 +0000

    Creating a wall of fire.

---
+++ Assets/MovingFire.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingFire : MonoBehaviour {
+
+    public float velocity = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.Find("Player");
+        Vector3 sidewaysVector = Vector3.left;
+        Vector3 projectedPlayer = Vector3.Project(player.transform.position, sidewaysVector);
+        Vector3 projectedFire = Vector3.Project(transform.position, sidewaysVector);
+        transform.position += projectedPlayer - projectedFire;
+        transform.position += Vector3.forward * Time.deltaTime * velocity;
+	}
+}
