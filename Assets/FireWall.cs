367ef8fd8c7296bc6b7ffbbd3705ad8d86345b79
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-06 23:08:09 +0000

    VRB12 Creating a Froggy Inferno

---
+++ Assets/FireWall.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.Find("Player");
+        Vector3 delta = player.transform.position - transform.position;
+        Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
+        transform.position += projectedDelta;
+	}
+}
