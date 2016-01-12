044e20a3e6c6ba1e01ebcfc8c2f5b06bc28cea1a
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-12 22:23:36 +0000

    VRB13 Motivating the Player to Move

--- Assets/FireWall.cs
+++ Assets/FireWall.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FireWall : MonoBehaviour {
 
+    public float creepSpeed = 0.01f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,9 +12,20 @@ public class FireWall : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        FollowPlayer();
+        CreepForward();
+	}
+
+    void FollowPlayer ()
+    {
         GameObject player = GameObject.Find("Player");
         Vector3 delta = player.transform.position - transform.position;
         Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
         transform.position += projectedDelta;
-	}
+    }
+
+    void CreepForward ()
+    {
+        transform.position += Vector3.forward * creepSpeed;
+    }
 }
