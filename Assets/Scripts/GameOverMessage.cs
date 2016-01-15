2516d454e6c3d845c33b3a2ce0f90ad3a470e716
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-15 16:51:40 +0000

    Display on gameover. And some button prefabs.

--- Assets/Scripts/GameOverMessage.cs
+++ Assets/Scripts/GameOverMessage.cs
@@ -6,17 +6,26 @@ public class GameOverMessage : MonoBehaviour {
     public float UIDistance = 3.0f;
     public float UIHight = 1.0f;
     private Player player;
+    private Canvas canvas;
+    private GameState state;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindObjectOfType<Player>();
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+        state = GameObject.FindObjectOfType<GameState>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.rotation = Quaternion.LookRotation(player.LookingDirection());
-        transform.position = player.transform.position;
-        transform.position += player.LookingDirection() * UIDistance;
-        transform.position += Vector3.up * UIHight;
+        if (state.IsGameOver)
+        {
+            canvas.enabled = true;
+            transform.rotation = Quaternion.LookRotation(player.LookingDirection());
+            transform.position = player.transform.position;
+            transform.position += player.LookingDirection() * UIDistance;
+            transform.position += Vector3.up * UIHight;
+        }
 	}
 }
