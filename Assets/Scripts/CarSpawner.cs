9b294ab26d4eb730bd979dd48ea68ecea3774e81
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-17 14:07:26 +0000

    Car spawner makes one car. Car updates position and dies after time dictated by length.

---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour {
+
+    public GameObject[] vehiclePrefab;
+    public float startHeight = 1.0f;
+    public float startOffset = -500.0f;
+    public float finishOffset = 500.0f;
+    public float laneSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start() {
+        InstantiateVehicle(0);
+    }
+
+    void InstantiateVehicle(int index)
+    {
+        float width = finishOffset - startOffset;
+
+        GameObject vehicleObject = (GameObject)Instantiate(vehiclePrefab[0], SpawnPosition(), transform.rotation);
+        vehicleObject.transform.parent = transform;
+
+        Vehicle vehicle = vehicleObject.GetComponent<Vehicle>();
+        vehicle.SetPath(laneSpeed, width);
+    }
+
+    Vector3 SpawnPosition()
+    {
+        Vector3 startPosition = transform.position;
+        startPosition -= Vector3.left * startOffset;
+        startPosition += Vector3.up * startHeight;
+        return startPosition;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+
+    private float speed;
+    private float length;
+
+	// Use this for initialization
+	void Start () {
+        float lifeSpanTime = length / speed;
+        Invoke("Remove", lifeSpanTime);
+    }
+
+    void Remove ()
+    {
+        Destroy(gameObject);
+    }
+
+    public void SetPath(float someSpeed, float someLength)
+    {
+        speed = someSpeed;
+        length = someLength;
+    }
+
+    void Update ()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
