b8af2360df0eff671f1f41cac336f44b74bf95bf
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-24 12:47:48 +0000

    VRB21 Spawning the Vehicles

---
+++ Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+    public GameObject[] vehiclePrefabs;
+    public float heightOffset = 1f;
+    public float startOffset = -10f;
+
+    // Use this for initialization
+    void Start() {
+        Vector3 positionOffset = transform.position;
+        positionOffset += heightOffset * Vector3.up;
+        positionOffset += startOffset * Vector3.right;
+        GameObject vehicleObject = Instantiate(vehiclePrefabs[0]);
+        vehicleObject.transform.position = positionOffset;
+        vehicleObject.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
