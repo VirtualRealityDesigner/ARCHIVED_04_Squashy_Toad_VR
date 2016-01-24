fde8ec08c820d77232bb2cacb98cf2d2bd1306b3
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-24 13:55:19 +0000

    VRB22 Making the Vehicles Move

--- Assets/Scripts/VehicleSpawner.cs
+++ Assets/Scripts/VehicleSpawner.cs
@@ -6,19 +6,33 @@ public class VehicleSpawner : MonoBehaviour {
     public GameObject[] vehiclePrefabs;
     public float heightOffset = 1f;
     public float startOffset = -10f;
+    public float speed = 5.0f;
 
     // Use this for initialization
     void Start() {
+        InstantiateVehicle();
+	}
+
+    void InstantiateVehicle()
+    {
+        GameObject vehicleObject = Instantiate(vehiclePrefabs[0]);
+        vehicleObject.transform.position = GetPositionOffset();
+        vehicleObject.transform.parent = transform;
+
+        Vehicle vehicleComponent = vehicleObject.GetComponent<Vehicle>();
+        vehicleComponent.SetSpeed(speed);
+    }
+
+    Vector3 GetPositionOffset()
+    {
         Vector3 positionOffset = transform.position;
         positionOffset += heightOffset * Vector3.up;
         positionOffset += startOffset * Vector3.right;
-        GameObject vehicleObject = Instantiate(vehiclePrefabs[0]);
-        vehicleObject.transform.position = positionOffset;
-        vehicleObject.transform.parent = transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        return positionOffset;
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
