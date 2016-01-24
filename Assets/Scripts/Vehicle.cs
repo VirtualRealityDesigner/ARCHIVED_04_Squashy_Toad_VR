79a729b2980cd444fe062e9a1347d7076bf7b20c
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-01-24 13:55:19 +0000

    VRB22 Making the Vehicles Move

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+
+    private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void SetSpeed(float someSpeed)
+    {
+        speed = someSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+}
