10efb4c555a5db7ffc80022d7d0874c5cd613e7d
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-02-22 17:02:55 +0000

    VRB26 Squashy Car Death

---
+++ Assets/Scripts/LethalCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LethalCollider : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            GameState state = GameObject.FindObjectOfType<GameState>();
+            state.IsGameOver = true;
+        }
+    }
+}
