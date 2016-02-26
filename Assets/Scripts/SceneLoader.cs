f90ced29177343dbde26af9d9e79f9ffc9e53748
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-02-26 14:00:49 +0000

    VRB32 Creating a Splash Screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+    public float loadLevelAfterTime = 0;
+
+    // Use this for initialization
+    void Start() {
+        if (loadLevelAfterTime > 0)
+        {
+            Invoke("LoadNextLevel", loadLevelAfterTime);
+        }
+	}
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
