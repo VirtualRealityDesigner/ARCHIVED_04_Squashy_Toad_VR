d6f2a6194ab9d557d057e0ef0de895e5e1b9c1c0
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-02-27 10:48:40 +0000

    VRB35 Destroying Burnt Lanes

---
+++ Assets/Scripts/FireDestroyer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+    {
+        Destroy(collider.gameObject);
+    }
+}
